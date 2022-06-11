47d35895a32e972cf82b4c7f1b54a0d4b74f60c8
Rebkova <rebkova@gmail.com>
2022-06-11 23:09:51 +0200

    Add components folder and a PublicCocktailList with some notes

---
+++ src/components/PublicCocktailList.tsx
@@ -0,0 +1,6 @@
+// renders the whole list everybody can see (from the API)
+// StartPage contains Filter Section and CocktailList components
+
+// StartPage has a list of cocktails
+// one can filter based on all/alcohol/non-alcohol drinks
+// 
